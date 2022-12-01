0ce21d81c82028465d77ad2c52083ae4066d49fb
Prince <prince.mst13@itbhu.ac.in>
2022-12-01 14:38:59 +0530

    add team and fantasyteam schema

---
+++ src/models/fantasy_team.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const FantasyTeamSchema = mongoose.Schema({
+
+    short_name : {
+        type : String,
+        required: true
+    }, 
+    full_name : {
+        type : String,
+        required: true
+    },   
+    wicketkeepers: {
+        type: [String],
+        required: true
+    },
+    batters: {
+        type: [String],
+        required: true
+    },
+    bowlers: {
+        type: [String],
+        required: true
+    },
+    all_rounders: {
+        type: [String],
+        required: true
+    },
+    logo_url : {
+        type : String,
+    }, 
+
+}, {timestamps : true});
+
+module.exports = mongoose.model("fantasy_team", FantasyTeamSchema);
